ddd2096ddd867e7baed152ff26a9c9978e3c70dd
“[firstname <faith.gacheri@student.moringaschool.com>
2022-11-24 10:31:33 +0300

    Completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    # added rescue_from
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -9,9 +11,10 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
-  end
+   # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+   bird = Bird.create!(bird_params)
+   render json: bird, status: :created
+ end
 
   # GET /birds/:id
   def show
@@ -21,10 +24,11 @@ class BirdsController < ApplicationController
 
   # PATCH /birds/:id
   def update
-    bird = find_bird
-    bird.update(bird_params)
-    render json: bird
-  end
+  bird = find_bird
+  # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+  bird.update!(bird_params)
+  render json: bird
+end
 
   # DELETE /birds/:id
   def destroy
@@ -47,4 +51,8 @@ class BirdsController < ApplicationController
     render json: { error: "Bird not found" }, status: :not_found
   end
 
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
+
